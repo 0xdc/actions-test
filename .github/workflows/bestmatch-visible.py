b9466db2777763fc329c90dd9678f6f70e51d538
0xdc <github@0xdc.xyz>
2024-11-21 20:43:06 +0000

    adjust action log groups

--- .github/workflows/bestmatch-visible.py
+++ .github/workflows/bestmatch-visible.py
@@ -7,12 +7,12 @@ import portage
 p = portage.db[portage.root]["porttree"].dbapi
 is_github = 'GITHUB_STEP_SUMMARY' in os.environ
 
-if is_github: sys.stderr.write(f"::group::{sys.argv[1]}\n")
+if is_github: print(f"::group::{sys.argv[1]}", file=sys.stderr)
 
 bestmatch = p.xmatch("bestmatch-visible", sys.argv[1])
 print(bestmatch)
 
 if is_github:
     with open(os.environ['GITHUB_STEP_SUMMARY'], 'a') as summary:
-        summary.write(f'- found version {bestmatch}\n')
-    sys.stderr.write("::endgroup::\n")
+        print(f'- found version {bestmatch}', file=summary)
+    print("::endgroup::", file=sys.stderr)
