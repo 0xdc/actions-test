bbacd7389ca8be43727d3715cd73adf01e597b34
0xdc <github@0xdc.xyz>
2024-11-21 16:49:59 +0000

    group log lines

--- .github/workflows/bestmatch-visible.py
+++ .github/workflows/bestmatch-visible.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 
+import os
 import sys
 import portage
 
 p = portage.db[portage.root]["porttree"].dbapi
 
-print( p.xmatch("bestmatch-visible", sys.argv[1]) )
+print( "::group::bestmatch-visible" )
+bestmatch = p.xmatch("bestmatch-visible", sys.argv[1])
+with open(os.environ['GITHUB_STEP_SUMMARY'], 'a') as summary:
+    print(bestmatch)
+    summary.write(f'found version {bestmatch}\n')
+print( "::endgroup::" )
