b99580881eba2e6b0924fa2e4ebed02a77e2ee79
0xdc <github@0xdc.xyz>
2024-11-21 19:35:27 +0000

    re-use output from bestmatch-visible.py

--- .github/workflows/bestmatch-visible.py
+++ .github/workflows/bestmatch-visible.py
@@ -7,7 +7,7 @@ import portage
 p = portage.db[portage.root]["porttree"].dbapi
 is_github = 'GITHUB_STEP_SUMMARY' in os.environ
 
-if is_github: print("::group::bestmatch-visible")
+if is_github: sys.stderr.write(f"::group::{sys.argv[1]}\n")
 
 bestmatch = p.xmatch("bestmatch-visible", sys.argv[1])
 print(bestmatch)
@@ -15,4 +15,4 @@ print(bestmatch)
 if is_github:
     with open(os.environ['GITHUB_STEP_SUMMARY'], 'a') as summary:
         summary.write(f'found version {bestmatch}\n')
-    print("::endgroup::")
+    sys.stderr.write("::endgroup::\n")
