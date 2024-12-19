659369664af8068550d64ce1b3e97d3399a2f50a
0xdc <github@0xdc.xyz>
2024-12-19 13:43:45 +0000

    kernel: use allnoconfig
    smaller set of options for hopefully quicker build.

--- kernel.nix
+++ kernel.nix
@@ -1,6 +1,7 @@
 {
 	packageOverrides = pkgs: {
 		linux = pkgs.linux.override {
+			defconfig = "allnoconfig";
 			kernelPatches = [{
 				name = "lte-screen-quirk";
 				patch = ./10IGL5-LTE_screen_quirk.patch;
